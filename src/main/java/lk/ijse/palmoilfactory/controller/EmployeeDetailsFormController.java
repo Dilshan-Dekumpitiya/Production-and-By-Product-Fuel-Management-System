0a22f33e9342d9bd2dc8d5852b51156e7e523eec
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-04-05 17:23:15 +0530

    Employee updated

--- src/main/java/lk/ijse/palmoilfactory/controller/EmployeeDetailsFormController.java
+++ src/main/java/lk/ijse/palmoilfactory/controller/EmployeeDetailsFormController.java
@@ -3,11 +3,19 @@ package lk.ijse.palmoilfactory.controller;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import lk.ijse.palmoilfactory.model.EmployeeModel;
+import lk.ijse.palmoilfactory.model.SupplierModel;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class EmployeeDetailsFormController implements Initializable {
@@ -27,14 +35,43 @@ public class EmployeeDetailsFormController implements Initializable {
     private JFXTextField txtEmployeeSalary;
 
     @FXML
-    private JFXComboBox<?> cmbEmployeetype;
+    private JFXComboBox<String> cmbEmployeetype;
 
     @FXML
-    private JFXComboBox<?> cmbEmployeeSchId;
+    private JFXComboBox<String> cmbEmployeeSchId;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Platform.runLater(() -> txtEmployeeId.requestFocus());
+        loadSchIds();
+        loadEmpType();
+    }
+
+    private void loadEmpType() {
+        ArrayList<String> empTypeList=new ArrayList<> ();
+        empTypeList.add("Oil Production");
+        empTypeList.add("By Production");
+        empTypeList.add("Other");
+
+        ObservableList<String> dataSet = FXCollections.observableArrayList(empTypeList);
+        cmbEmployeetype.setItems(dataSet);
+
+    }
+
+    private void loadSchIds() {
+        try {
+            ObservableList<String> obList = FXCollections.observableArrayList();
+            List<String> schIDs = EmployeeModel.getSchIDs();
+
+            for (String schId : schIDs) {
+                obList.add(schId);
+            }
+            cmbEmployeeSchId.setItems(obList);
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "OOPSSS!! something happened!!!").show();
+        } catch (ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, "OOPSSS!! something happened!!!").show();
+        }
     }
 
     @FXML
---
+++ src/main/java/lk/ijse/palmoilfactory/model/EmployeeModel.java
@@ -0,0 +1,24 @@
+package lk.ijse.palmoilfactory.model;
+
+import lk.ijse.palmoilfactory.util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeModel {
+    public static List<String> getSchIDs() throws SQLException, ClassNotFoundException {
+        List<String> schIds = new ArrayList<>();
+
+        String sql = "SELECT schId FROM schedule";
+
+        ResultSet resultSet= CrudUtil.execute(sql);
+
+        while(resultSet.next()) {
+            schIds.add(resultSet.getString(1));
+        }
+        return schIds;
+
+    }
+}
