0fb33c750ba30f8c9b32b2f2251eee247e23e88f
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-04-20 15:10:32 +0530

    Regex add to login

---
+++ src/main/java/lk/ijse/palmoilfactory/util/Regex.java
@@ -0,0 +1,17 @@
+package lk.ijse.palmoilfactory.util;
+
+
+public class Regex {
+    private static final String USERNAME_REGEX = "^[A-Za-z0-9]{3,}$";
+    private static final String PASSWORD_REGEX = "[aA-zZ0-9]{8,20}$";
+
+    public static boolean validateUsername(String username) {
+        return username.matches(USERNAME_REGEX);
+    }
+
+    public static boolean validatePassword(String password) {
+        return password.matches(PASSWORD_REGEX);
+    }
+
+
+}
